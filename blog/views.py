5330479729a9017f83882517202935a558b22aca
ali2tehrani <ansari_javascript2@yahoo.com>
2025-10-01 07:43:24 +0000

    add post edit

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def post_list(request):
     '''
@@ -22,10 +23,33 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            my_user=User.objects.get(username='root')
+            post.author = my_user
+            '''
             post.author = request.user
+            '''
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            my_user=User.objects.get(username='root')
+            post.author = my_user
+            '''
+            post.author = request.user
+            '''
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
